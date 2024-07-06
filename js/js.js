f982e0a18e7ad7b455cac0342d03f94fce86b378
Yershov Artem <fortestsante@gmail.com>
2024-07-06 22:59:16 +0300

    refactor: changed structure for task

--- js/js.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', function (e) {
-	const targetElement = e.target;
-	if (targetElement.closest('.button-burger')) {
-		document.documentElement.classList.toggle('menu-open');
-	}
-})
